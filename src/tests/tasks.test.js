5f836983d805abf8798edb54bafc22e9d10fb1cb
Codeprince <anyaohaprince122@gmail.com>
2022-02-15 15:13:59 +0100

    added flag to make tests fun in seires

---
+++ src/tests/tasks.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const Task = require("../models/task");
+const app = require("../app");
+// const User = require("../models/user");
+const { user1, setUpDatabase } = require("./fixtures/db");
+
+beforeEach(setUpDatabase);
+test;
