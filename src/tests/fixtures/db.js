b5a835641456fcd19f6a5d7f8923e6afb68a2df3
Codeprince <anyaohaprince122@gmail.com>
2022-02-15 15:28:52 +0100

    created second user

--- src/tests/fixtures/db.js
+++ src/tests/fixtures/db.js
@@ -12,9 +12,19 @@ const user1 = {
   tokens: [{ token: jwt.sign({ _id: user1Id }, process.env.JWT_SECRET) }],
 };
 
+const user3Id = new mongoose.Types.ObjectId();
+
+const user3 = {
+  _id: user3Id,
+  name: "testname3",
+  email: "testemail3@gmail.com",
+  password: "testpass3",
+  tokens: [{ token: jwt.sign({ _id: user1Id }, process.env.JWT_SECRET) }],
+};
 const setUpDatabase = async () => {
   await User.deleteMany();
   await new User(user1).save();
+  await new User(user3).save();
 };
 
 module.exports = {
