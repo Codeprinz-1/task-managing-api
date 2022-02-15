bab9eb92fa338d9e6a5c310ddfc13b4489ff31b4
Codeprince <anyaohaprince122@gmail.com>
2022-02-15 15:21:37 +0100

    added test for creating task

--- src/tests/tasks.test.js
+++ src/tests/tasks.test.js
@@ -5,4 +5,17 @@ const app = require("../app");
 const { user1, setUpDatabase } = require("./fixtures/db");
 
 beforeEach(setUpDatabase);
-test;
+
+test("Should create task for user ", async () => {
+  const response = await request(app)
+    .post("/tasks")
+    .set("Authorization", `Bearer ${user1.tokens[0].token}`)
+    .send({
+      description: "Change the world",
+    })
+    .expect(201);
+
+  const task = await Task.findById(response.body._id);
+  expect(task).not.toBeNull();
+  expect(task.completed).toEqual(false);
+});
