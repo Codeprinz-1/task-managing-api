fda6cc17002f1c92e43eba2eb021fa5d13895e2f
Codeprince <anyaohaprince122@gmail.com>
2022-02-15 09:20:32 +0100

    added should sign up a new user test

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,2 +1,13 @@
 const request = require("supertest");
 const app = require("../app");
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "andrew",
+      email: "andrew@mead.com",
+      password: "my favourite password",
+    })
+    .expect(201);
+});
