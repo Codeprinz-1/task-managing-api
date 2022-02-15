331c3f6db75bdfd6f8c8725055432ebf8e7be3d3
Codeprince <anyaohaprince122@gmail.com>
2022-02-15 10:14:08 +0100

    imported user model

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,5 +1,10 @@
 const request = require("supertest");
 const app = require("../app");
+const User - require('../models/user')
+
+beforeEach(() => {
+
+})
 
 test("Should signup a new user", async () => {
   await request(app)
